0ea49d529761ab725e2ab3670610c66263bfdb72
Paulo <Paulo>
2017-05-03 12:07:56 -0300

    Operacoes

--- app/src/main/java/com/mimduim/manualcalculadora/CalculadoraActivity.java
+++ app/src/main/java/com/mimduim/manualcalculadora/CalculadoraActivity.java
@@ -1,14 +1,14 @@
 package com.mimduim.manualcalculadora;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TableLayout;
-        import android.widget.TableRow;
-        import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 public class CalculadoraActivity extends AppCompatActivity {
 
@@ -16,19 +16,32 @@ public class CalculadoraActivity extends AppCompatActivity {
     private Button botao;
     private TableRow linha;
     private TableLayout leiaute;
+    private float num1, num2;
+    private String STRING = "";
+    private String aux, textvisor, numero;
+    private float resp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         visor = new EditText(this);
-         visor.setText("");
+        visor = new EditText(this);
+        visor.setText("");
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Button btn = (Button) view;
-                tecladoCalculadora(btn.getText().toString());
                 visor.setCursorVisible(false);
-            }};
+                visor.setText(tecladoCalculadora(btn.getText().toString()));
+                //OPERACOES(btn);
+            }
+        };
+
+
+        TABLECREATE(listener);
+
+
+
        /* View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,21 +50,22 @@ public class CalculadoraActivity extends AppCompatActivity {
             }
         };*/
 
-        leiaute = new TableLayout(this);
 
+    }
+
+    public void TABLECREATE(View.OnClickListener listener) {
+        leiaute = new TableLayout(this);
         leiaute.setLayoutParams(
                 new ViewGroup.LayoutParams(
                         ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.MATCH_PARENT));
-
         leiaute.addView(visor);
         leiaute.setStretchAllColumns(true);
-
         int cont = 7;
         for (int i = 0; i < 5; i++) {
-             linha = new TableRow(this);
+            linha = new TableRow(this);
             for (int j = 0; j < 4; j++) {
-                 botao = new Button(this);
+                botao = new Button(this);
                 if (i == 0 && j == 0)
                     botao.setText("C");
                 else if (i == 0 && j == 1)
@@ -80,24 +94,104 @@ public class CalculadoraActivity extends AppCompatActivity {
                 linha.addView(botao);
             }
             if (i > 0)
-            cont-=6;
+                cont -= 6;
             leiaute.addView(linha);
         }
         setContentView(leiaute);
     }
 
-    public void tecladoCalculadora(String lerNumero){
-        String aux = null;
-        if (visor.getText().toString().trim().equals("")) {
-            String textvisor = visor.getText().toString();
-            String numero = String.valueOf(lerNumero);
-            aux = textvisor + numero;
-            visor.setText(aux);
-        } else {
-            String textvisor = visor.getText().toString();
-            String numero = String.valueOf(lerNumero);
-            aux = textvisor + numero;
-            visor.setText(aux);
+    public String ADICAO() {
+
+        if (numero == "+") {
+            aux = textvisor;
+            STRING = aux;
+            aux = "";
+            return aux;
         }
+        if (numero == "=") {
+            num1 = Float.parseFloat(STRING);
+            num2 = Float.parseFloat(textvisor);
+            resp = num1 + num2;
+            aux = String.valueOf(resp);
+            return aux;
+        }
+        aux = textvisor + numero;
+        return aux;
+    }
+
+    public String SUBTRACAO() {
+
+        if (numero == "-") {
+            aux = textvisor;
+            STRING = aux;
+            aux = "";
+            return aux;
+        }
+        if (numero == "=") {
+            num1 = Float.parseFloat(STRING);
+            num2 = Float.parseFloat(textvisor);
+            resp = num1 - num2;
+            aux = String.valueOf(resp);
+            return aux;
+        }
+        aux = textvisor + numero;
+        return aux;
+    }
+
+    public String DIVISAO() {
+
+        if (numero == "/") {
+            aux = textvisor;
+            STRING = aux;
+            aux = "";
+            return aux;
+        }
+        if (numero == "=") {
+            num1 = Float.parseFloat(STRING);
+            num2 = Float.parseFloat(textvisor);
+            resp = num1 / num2;
+            aux = String.valueOf(resp);
+            return aux;
+        }
+        aux = textvisor + numero;
+        return aux;
+    }
+
+    public String MULTIPLICACAO() {
+
+        if (numero == "*") {
+            aux = textvisor;
+            STRING = aux;
+            aux = "";
+            return aux;
+        }
+        if (numero == "=") {
+            num1 = Float.parseFloat(STRING);
+            num2 = Float.parseFloat(textvisor);
+            resp = num1 * num2;
+            aux = String.valueOf(resp);
+            return aux;
+        }
+        aux = textvisor + numero;
+        return aux;
+    }
+
+    public String tecladoCalculadora(String string) {
+        aux = null;
+        textvisor = visor.getText().toString();
+        numero = string;
+
+        if (numero == "+") {
+            return ADICAO();
+        } else if (numero == "-") {
+            return SUBTRACAO();
+        } else if (numero == "/") {
+            return DIVISAO();
+        } else if (numero == "*") {
+            return MULTIPLICACAO();
+        }
+
+        aux = textvisor + numero;
+        return aux;
     }
 }
